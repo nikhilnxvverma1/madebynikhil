f1d238be7c9d62eb42801dd1524d267f35765850
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-08-12 17:37:01 +0530

    Introduced a new landing page and changed the favicon

--- routes/views/index.js
+++ routes/views/index.js
@@ -10,6 +10,6 @@ exports = module.exports = function(req, res) {
 	locals.section = 'home';
 	
 	// Render the view
-	view.render('landing-page',{layout:"header-content-footer.hbs"});
+	view.render('landing-page2',{layout:"content-footer.hbs"});
 	
 };
