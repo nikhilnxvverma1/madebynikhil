d2c30c61e3b4977cfc82aeecf343bff65c66900f
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-12-16 23:49:09 +0530

    Migrating to a static hugo project. Project undergoing framework change

--- typings/browser.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="browser/ambient/handlebars/handlebars.d.ts" />
-/// <reference path="browser/ambient/node/node.d.ts" />
--- typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/handlebars/handlebars.d.ts" />
-/// <reference path="main/ambient/node/node.d.ts" />
